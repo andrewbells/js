de5d8fd702c5a2daa1ab1c86296045ad65c13165
Andrey K aser <andkolok@gmail.com>
2013-10-20 23:49:54 +0300

    js timers

--- website/acerscript.js
+++ website/acerscript.js
@@ -5,15 +5,66 @@ document.onclick = function() {
 */
 
 function prepareEventHandlers(){
-	var myImage = document.getElementById('mainImage');
+	var myImage = document.getElementById('reportImage');
 
 	myImage.onclick = function() {
 		alert('you clicked an image');
 	}
+	
+	// the following can be put outside this function if script is put at the end of the document
+	var emailField = document.getElementById("email");
+
+	emailField.onfocus = function() {
+		if ( emailField.value == "your email") {
+			emailField.value = "";
+		}
+};
+
+	emailField.onblur = function() {
+		if ( emailField.value == "") {
+			emailField.value = "your email";
+		}
+	};
+}
+
+var messageArray = ["Best!", "Best of the Best!", "Best of the Best of the Best!"];
+var messageIndex = 0;
+/*
+function deeperFunction() {
+	// perform loop
+	for (var i = 0; i < 500; i++) {
+		// do stuff
+		var foo = i * (Math.random());
+		var bar = foo;
+		// more code
+	
+	}
+}
+*/
+
+function simpleFunction() {
+	// jump into a deeper function
+	// deeperFunction();
+	// now grab the message and change it
+	var newMessage = messageArray[messageIndex];
+	var messageElement = document.getElementById("mainMessage");
+	messageElement.innerHTML = newMessage;
+	messageIndex++;
+	if (messageIndex >= messageArray.length) {
+		messageIndex = 0;
+	}
+
+}
+
+
+function changeMessage() {
+	simpleFunction();
 }
 
 
 //window contains document
 window.onload = function() {
 	prepareEventHandlers();
-}
+	setInterval(changeMessage, 4000);
+};
+
