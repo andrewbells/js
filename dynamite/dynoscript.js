d3dc5d8b1de9d1498c8841c58209bc94c20feaf2
Andrey K aser <andkolok@gmail.com>
2013-10-22 23:07:23 +0300

    disarm button

--- dynamite/dynoscript.js
+++ dynamite/dynoscript.js
@@ -1,12 +1,15 @@
 
 function newGame() {
-	disarmCount = 1;
+	pressedDisarm = false;
+	pressedConfirm = false;
 	i = Math.floor((Math.random()*70)+30); 
 	fire = [true, true, true];
 	fire[Math.floor(Math.random()*3)] = false;
+	
 	fire_1 = fire[0];
 	fire_2 = fire[1];
 	fire_3 = fire[2];
+	
 	clicked_1 = false;
 	clicked_2 = false;
 	clicked_3 = false;
@@ -17,8 +20,7 @@ function newGame() {
 	switch_3.setAttribute("src", "_img/switch_on.jpg");
 	game++;
 	document.getElementById("infofield_1").innerHTML = game;
-	console.log('game');
-	console.log(game);
+	
 }
 
 window.onload = function() {
@@ -28,10 +30,21 @@ window.onload = function() {
 document.getElementById("newgame").onclick = function() {
 	newGame();
 };
+var pressedDisarm = false;
+var pressedConfirm = false;
 
 var win = 0;
 var lose = 0;
 var game = 0;
+
+var clicked_1 = false;
+var clicked_2 = false;
+var clicked_3 = false;
+
+var fire = [true, true, true];
+var fire_1 = fire[0];
+var fire_2 = fire[1];
+var fire_3 = fire[2];
 
 var switch_1 = document.getElementById("switch_1");
 var switch_2 = document.getElementById("switch_2");
@@ -99,69 +112,72 @@ switch_3.onclick = function() {
 	
 };
 
-var disarm = document.getElementById("disarm");
+var confirm = document.getElementById("confirm");
 var dynomite = document.getElementById("dynomite");
 
-disarm.onclick = function() {
+confirm.onclick = function() {
 
-	if (clicked_3 == true && disarmCount == 1) {
+	if (clicked_3 == true && pressedConfirm == false) {
 		if (fire_2 == true) {
 		switch_2.setAttribute("src", "_img/switch_off.jpg");
 		clicked_2 = true;
-		message.innerHTML = "Look, switch 2 is not the safe one - don't forget to switch it back, wanna change your choice?";
+		message.innerHTML = "Look, switch 2 is not the safe one - don't forget to switch it back, wanna change your choice? When you are done hit 'disarm'";
 		
 		}
 		else if (fire_1 == true && clicked_2 == false) {
 		switch_1.setAttribute("src", "_img/switch_off.jpg");
 		clicked_1 = true;
-		message.innerHTML = "Look, switch 1 is not the safe one - don't forget to switch it back, wanna change your choice?";
+		message.innerHTML = "Look, switch 1 is not the safe one - don't forget to switch it back, wanna change your choice? When you are done hit 'disarm'";
 		
 		}
-		
+		pressedConfirm = true;
 	}
-	else if (clicked_2 == true && disarmCount == 1) {
+	else if (clicked_2 == true && pressedConfirm == false) {
 		if (fire_3 == true) {
 		switch_3.setAttribute("src", "_img/switch_off.jpg");
 		clicked_3 = true;
-		message.innerHTML = "Look, switch 3 is not the safe one - don't forget to switch it back, wanna change your choice?";
+		message.innerHTML = "Look, switch 3 is not the safe one - don't forget to switch it back, wanna change your choice? When you are done hit 'disarm'";
 		}
 		else if (fire_1 == true && clicked_3 == false) {
 		switch_1.setAttribute("src", "_img/switch_off.jpg");
 		clicked_1 = true
-		message.innerHTML = "Look, switch 1 is not the safe one - don't forget to switch it back, wanna change your choice?";
+		message.innerHTML = "Look, switch 1 is not the safe one - don't forget to switch it back, wanna change your choice? When you are done hit 'disarm'";
 		}
-		
+		pressedConfirm = true;
 	}
-	else if (clicked_1 == true && disarmCount == 1) {
+	else if (clicked_1 == true && pressedConfirm == false) {
 		if (fire_2 == true) {
 		switch_2.setAttribute("src", "_img/switch_off.jpg");
 		clicked_2 = true;
-		message.innerHTML = "Look, switch 2 is not the safe one - don't forget to switch it back, wanna change your choice?";
+		message.innerHTML = "Look, switch 2 is not the safe one - don't forget to switch it back, wanna change your choice? When you are done hit 'disarm'";
 		}
 		else if (fire_3 == true && clicked_2 == false) {
 		switch_3.setAttribute("src", "_img/switch_off.jpg");
 		clicked_3 = true
-		message.innerHTML = "Look, switch 3 is not the safe one - don't forget to switch it back, wanna change your choice?";
+		message.innerHTML = "Look, switch 3 is not the safe one - don't forget to switch it back, wanna change your choice? When you are done hit 'disarm'";
 		}
-		
+		pressedConfirm = true;
 	}
-	disarmCount++;
+	console.log(fire);
+};
+
+document.getElementById("disarm").onclick = function() {
 	
-	if (disarmCount == 3 && ((clicked_3 == true && fire_3 == true) || (clicked_2 == true && fire_2 == true) || (clicked_1 == true && fire_1 == true))) {
+	if (pressedDisarm == false && ((clicked_3 == true && fire_3 == true) || (clicked_2 == true && fire_2 == true) || (clicked_1 == true && fire_1 == true))) {
 		dynomite.setAttribute("src", "_img/explosion.jpg");
 		i = "wrong switch!";
 		lose++;
 		document.getElementById("infofield_3").innerHTML = lose;
 		message.innerHTML = "Not this time, dude, try once more...";
-		
+		pressedDisarm = true;
 	}
-	else if (disarmCount == 3 && ((clicked_3 == true && fire_3 == false) || (clicked_2 == true && fire_2 == false) || (clicked_1 == true && fire_1 == false))) {
+	else if (pressedDisarm == false && ((clicked_3 == true && fire_3 == false) || (clicked_2 == true && fire_2 == false) || (clicked_1 == true && fire_1 == false))) {
 		dynomite.setAttribute("src", "_img/sorry_mario.jpg");
 		i = "Success!";
 		win++;
 		document.getElementById("infofield_2").innerHTML = win;
 		message.innerHTML = "Congratulations, you've disarmed the bomb!";
-		
+		pressedDisarm = true;
 	}
 	
 };
